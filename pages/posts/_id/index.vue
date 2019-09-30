771e5fd5020c73d3c1008e29d04320f9ae96a37b
jajan20 <jamie.jansen2@hva.nl>
2019-09-30 11:13:23 +0200

    added post id route, fixed styling

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="single-post">
+        <p>01-01-2019</p>
+        <p><strong>Start time:</strong>09:00</p>
+        <p><strong>End time:</strong>19:20</p>
+        <p><strong>Lunch time:</strong>01:00h</p>
+        <p><strong>Clocked:</strong>Yes</p>
+        <p><strong>Total:</strong>Worked 9 hours and 20 minutes.</p>
+    </section>
+</template>
+
+<style scoped>
+.single-post {
+    border: solid 1px black;
+    width: 30%;
+    margin: 10px;
+    padding: 10px;
+}
+</style>
