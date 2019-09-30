e073a7cb8656d016e53da56b16407ec80d0135f7
jajan20 <jamie.jansen2@hva.nl>
2019-09-30 11:06:14 +0200

    added post page + styling

---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="post-page">
+        <p>01-01-2019</p>
+        <p><strong>Start time:</strong>09:00</p>
+        <p><strong>End time:</strong>19:20</p>
+        <p><strong>Lunch time:</strong>01:00h</p>
+        <p><strong>Clocked:</strong>Yes</p>
+        <p><strong>Total:</strong>Worked 9 hours and 20 minutes.</p>
+    </section>
+</template>
+
+<style scoped>
+.post-page {
+    border: solid 1px black;
+    width: 30%;
+    margin: 10px;
+    padding: 10px;
+}
+</style>
