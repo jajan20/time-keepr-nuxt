e67da1dcbb8b60f4ff9d6e1ccc7d87ca74e98017
jajan20 <jamie.jansen2@hva.nl>
2019-09-30 13:20:08 +0200

    added preview post component, included props object

---
+++ components/posts/PostPreview.vue
@@ -0,0 +1,58 @@
+<template>
+    <nuxt-link :to="'/posts/' + id" class="link-container">
+        <article class="post-preview">
+            <p>{{ date }}</p>
+            <p>{{ workedHours }}</p>
+        </article>  
+    </nuxt-link>
+</template>      
+
+<script>
+export default {
+    name: 'PostPreview',
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true
+        },
+        workedHours: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.link-container {
+    display: inline;
+}
+
+.post-preview {
+  border: solid 1px black;
+  transition: background-color 150ms;
+  width: 25%;
+  margin-bottom: 10px;
+  padding: 10px;
+  display: inline-block;
+}
+
+a {
+  text-decoration: none;
+  color: lightslategray;
+  transition: color 150ms;
+}
+
+a:hover {
+  color: black;
+}
+
+.post-preview:hover {
+  background-color: lightslategray;
+
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="container">
-  <h1>Hello World</h1>
-  <p>Time Keepr is a simple app that will help you log your worked hours in a simple and efficient way</p>
-  <button>Continue</button>
+  <div class="home-page">
+    <section class="welcome-container">
+      <h1>Hello World</h1>
+      <p>Time Keepr is a simple app that will help you log your worked hours in a simple and efficient way</p>
+      <button>Continue</button>
+    </section>
+    <section class="overview">
+      <PostPreview id="1" date="01-01-2019" workedHours="09:20h"/>
+      <PostPreview id="2" date="05-01-2019" workedHours="08:15h"/>
+      <PostPreview id="3" date="09-01-2019" workedHours="03:30h"/>
+    </section>
   </div>
 </template>
 
 <script>
+import PostPreview from '../components/posts/PostPreview'
 
+export default {
+  components: {
+    PostPreview
+  }
+}
 </script>
 
-<style>
+<style scoped>
 </style>
