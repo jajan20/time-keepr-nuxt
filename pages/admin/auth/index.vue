c8a397db17c205f6a7ac50607146bdb27afd5313
jajan20 <jamie.jansen2@hva.nl>
2019-09-30 16:14:44 +0200

    experimented with auth

---
+++ pages/admin/auth/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="auth-page">
+        <div class="auth-container">
+            <form>
+                <AppInput type="email">Email</AppInput>
+                <AppInput type="email">Password</AppInput>
+                <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppInput from '@/components/UI/AppInput'
+import AppButton from '@/components/UI/AppButton'
+
+export default {
+    name: 'AuthPage',
+    data() {
+        return {
+            isLogin: true
+        }
+    },
+    components: {
+        AppInput,
+        AppButton
+    }
+}
+</script>
+
+<style scoped>
+.auth-page {
+    padding: 10px;
+}
+</style>
